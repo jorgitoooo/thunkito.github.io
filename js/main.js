9d4bb51af6a2d1fc5ce439e16ce0fa3ffb1a53ed
Jorge Garcia <jlg.sftdev@gmail.com>
2019-10-31 01:18:00 -0700

    Removed bfs from the algorithm visualizations

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ window.onload = () => {
     let stopId = requestAnimationFrame(scrollAnimation);
   };
 
-  const algorithms = [dijkstra, insertionSort, dfs, bfs, selectionSort];
+  const algorithms = [dijkstra, insertionSort, dfs, /* bfs, */ selectionSort];
   display(algorithms, 0);
 
   window.onscroll = highlightNavOnScroll;
